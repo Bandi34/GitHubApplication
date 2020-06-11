32ee6ce804060db74982366097b4222ca135e6f0
Bandi34 <dandi34@daum.net>
2020-06-11 14:53:10 +0900

    프로젝트 초기 등록

--- GitHubApplication/MainWindow.Designer.cs
+++ GitHubApplication/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GitHubApplication
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- GitHubApplication/MainWindow.cs
+++ GitHubApplication/MainWindow.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace GitHubApplication
 {
-    public partial class Form1 : Form
+
+    public partial class MainWindow : Form
     {
-        public Form1()
+
+        public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
+
 }
